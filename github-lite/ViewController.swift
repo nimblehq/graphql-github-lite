af2b6d8cf73fccf5b28455fd1417c293190148ac
Issarapong Poesua <dekablade01@gmail.com>
2017-11-16 21:17:24 +0700

    Initial project with cocoapods

---
+++ github-lite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  github-lite
+//
+//  Created by Issarapong Poesua on 11/16/2560 BE.
+//  Copyright © 2560 Issarapong Poesua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
